847ec045a13bf7d4359d9d6db5ea64f9703688c6
Fazle Rabbi Ador <fazle.rabbi@circlefintech.com>
2019-07-16 13:19:24 +0600

    first commit

---
+++ src/components/TouchId.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class TouchId extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> TouchId </Text>
+      </View>
+    );
+  }
+}
+
+export default TouchId;
